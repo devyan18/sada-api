bfe875ece54add086ae043e4a3d756784495dd5b
devyan18 <epache17@gmail.com>
2023-05-09 18:56:09 -0300

    api with fake data is works

--- src/api/routes/fake.routes.ts
+++ src/api/routes/fake.routes.ts
@@ -1,15 +1,8 @@
 import { Router } from 'express'
-import fs from 'fs'
-import path from 'path'
+import staticData from '../data/staticData'
 
 export const router = Router()
 
 router.get('/', (_req, res) => {
-  fs.readFile(path.join(__dirname, '../data/staticData.json'), 'utf8', (err, data) => {
-    if (err) {
-      console.log(err)
-      res.sendStatus(500)
-    }
-    res.json(JSON.parse(data))
-  })
+  res.json(staticData)
 })
