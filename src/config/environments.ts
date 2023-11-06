54bd3cc4908e3b4f2e9871b808ba56fe9f6b15b1
ASAPJJ <juanjosebrunelli@gmail.com>
2023-11-06 15:22:42 -0300

    me recibi de pendejo

--- src/config/environments.ts
+++ src/config/environments.ts
@@ -1,12 +1,12 @@
 export const environments = {
   port: parseInt(process.env.PORT || '0') || 3000,
   database: {
-    HOST: process.env.MYSQLHOST || 'localhost',
-    URL: process.env.MYSQL_URL || 'mysql://username:password@localhost:3306/database',
-    PASSWORD: process.env.MYSQLPASSWORD || 'juanjo124',
+    HOST: process.env.MYSQLHOST || '',
+    URL: process.env.MYSQL_URL || '',
+    PASSWORD: process.env.MYSQLPASSWORD || '',
     USER: process.env.MYSQLUSER || 'root',
     PORT: parseInt(process.env.MYSQLPORT || '0') || 3306,
-    DB: process.env.MYSQLDATABASE || 'ClimaDBv1',
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://ipfrecaf:recaf12@cluster0.rhyuqe1.mongodb.net/Prueba'
+    DB: process.env.MYSQLDATABASE || '',
+    MONGODB_URI: process.env.MONGODB_URI || ''
   }
 }
